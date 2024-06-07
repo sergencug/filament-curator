d7b3162bcaf7ee98e9b4662eee5a6d378be581a5
sergencug <sergencug@gmail.com>
2024-06-07 12:55:30 +0300

    tr lang

---
+++ resources/lang/tr/forms.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'sections' => [
+        'details' => 'Detaylar',
+        'exif' => 'Exif Verisi',
+        'file' => 'Dosya',
+        'meta' => 'Meta',
+        'preview' => 'Önizleme',
+        'upload_new' => 'Yeni Yükle',
+        'upload_new_helper' => 'Bu medya için herhangi bir düzenleme varsa, bunları yeniden oluşturmanız gerekebilir, bu otomatik olarak gerçekleşmez.',
+        'curation' => 'Düzenleme',
+    ],
+    'fields' => [
+        'alt' => 'Alt Metin',
+        'alt_hint' => 'Alt?',
+        'caption' => 'Başlık',
+        'description' => 'Açıklama',
+        'file' => 'Dosya',
+        'title' => 'Başlık',
+        'name' => 'İsim',
+    ],
+    'notices' => [
+        'success' => 'Öğe başarıyla güncellendi.',
+    ],
+    'curations' => [
+        'button_label' => 'Düzenleme Oluştur',
+    ],
+    'multi_upload' => [
+        'action_label' => 'Çoklu Yükleme',
+        'modal_heading' => 'Çoklu Dosya Yükle',
+        'modal_file_label' => 'Dosyalar',
+    ],
+];
---
+++ resources/lang/tr/notifications.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'update_success' => 'Kayıt başarıyla güncellendi.',
+    'update_error' => 'Kaydı güncellerken bir hata oluştu.',
+    'delete_success' => 'Kayıt başarıyla silindi.',
+    'delete_error' => 'Kaydı silerken bir hata oluştu.',
+];
